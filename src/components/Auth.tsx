289b6d944b8bacf7320f3a444fe7270523d6557f
aimlclubbytexl-cmd <aimlclub.bytexl@gmail.com>
2026-07-05 17:12:36 +0530

    Update Auth.tsx added logo

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -228,8 +228,6 @@ export const Auth: React.FC<AuthProps> = ({ onLogin }) => {
 };
 
 const CodeIcon = ({ size }: { size: number }) => (
-  <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <polyline points="16 18 22 12 16 6"></polyline>
-    <polyline points="8 6 2 12 8 18"></polyline>
-  </svg>
+  <img src="./public/aiml-logo.svg" alt="AI/ML Club Logo" width="100" height="100">
+
 );
